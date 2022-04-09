913a074f99e819a108d03d7a8cc54072c4d0a2a5
leanrosa1 <leanbmx1@gmail.com>
2022-04-09 20:01:14 -0300

    first commit

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        public static double Operar (Operando num1, Operando num2, char operador)
+        {
+            double ret = 0;
+            operador = ValidarOperador(operador);
+            
+            // PREGUNTAR
+            switch (operador)
+            {
+                case '+':
+                    ret = num1 + num2;
+                    break;
+                case '-':
+                    ret = num1 - num2;
+                    break;
+                case '/':
+                    ret = num1 / num2;
+                    break;
+                case '*':
+                    ret = num1 * num2;
+                    break;
+            }
+            return ret;
+        }
+
+        private static char ValidarOperador (char operador)
+        {
+            char ret;
+            if (operador != '+' && operador != '-' && operador != '/' && operador != '*')
+            {
+                ret = '+';
+            }
+            else
+            {
+                ret = operador;
+            }
+            return ret;
+        }
+    }
+}
---
+++ TP1/Entidades/Operando.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public Operando () :this(0)
+        {
+        }
+
+        public Operando (double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando (string strNumero) :this(double.Parse(strNumero))
+        {
+        }
+
+        private double ValidarOperando (string strNumero)
+        {
+            double ret;
+            bool isNumeric = double.TryParse(strNumero, out double numero);
+            if (isNumeric)
+            {
+                ret = numero;
+            }
+            else
+            {
+                ret = 0;
+            }
+            return ret;
+        }
+
+        private bool EsBinario (string binario)
+        {
+            foreach (char caracter in binario)
+            {
+                if (caracter != '0' && caracter != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public string BinarioDecimal (string binario)
+        {
+            string ret;
+            bool esBinario = this.EsBinario(binario);
+            if (esBinario)
+            {
+                ret = Convert.ToInt32(binario, 2).ToString();
+            }
+            else
+            {
+                ret = "Valor inválido";
+            }
+            return ret;
+        }
+
+        public string DecimalBinario (double numero)
+        {
+            string ret;
+            // PREGUNTAR: dice quedarse con la parte entera y absoluta
+            if (numero < 0)
+            {
+                ret = "Valor inválido";
+            }
+            else
+            {
+                int parteEntera = (int)(numero);
+                ret = Convert.ToString(parteEntera, 2);
+            }
+            return ret;
+        }
+
+        public string DecimalBinario (string numero)
+        {
+            string ret;
+            bool esNumerico = double.TryParse(numero, out double numeroAConvertir);
+            if (esNumerico)
+            {
+                ret = DecimalBinario(numeroAConvertir);
+            }
+            else
+            {
+                ret = "Valor inválido";
+            }
+            return ret;
+        }
+
+        public static double operator - (Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator + (Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator * (Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator / (Operando n1, Operando n2)
+        {
+            double ret;
+            if (n2.numero == 0)
+            {
+                ret = double.MinValue;
+            }
+            else
+            {
+                ret = n1.numero / n2.numero;
+            }
+            return ret;
+        }
+    }
+}
